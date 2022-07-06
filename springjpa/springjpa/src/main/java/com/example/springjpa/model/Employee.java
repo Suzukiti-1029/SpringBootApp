8870e82256e73a57fd5176fd0f35b62d10c0a3fe
Suzukiti-1029 <h40396suzujun@gmail.com>
2022-07-06 09:42:17 +0900

    Employeeモデルの作成

---
+++ springjpa/springjpa/src/main/java/com/example/springjpa/model/Employee.java
@@ -0,0 +1,24 @@
+package com.example.springjpa.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Employee {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @NotBlank
+  @Size(max = 40)
+  private String name;
+}
